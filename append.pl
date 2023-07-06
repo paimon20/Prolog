8c14371e684c956ec1540ac1d92ce78e40823668
Paimon <91016883+paimon20@users.noreply.github.com>
2023-07-06 19:32:04 +0530

    Create append.pl

---
+++ append.pl
@@ -0,0 +1,9 @@
+%write a prolog program to implement append for two list.
+
+list_member(X,[X|_]).
+list_member(X,[_|TAIL]):-
+    list_member(X,TAIL).
+
+list_append(A,T,T):-
+    list_member(A,T),!.
+list_append(A,T,[A|T]).
